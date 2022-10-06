3b765cddbdddf98a9f3f2bfb8559f3d17d771107
kangdokyoung <ehrud6672@naver.com>
2022-10-06 15:42:11 +0900

    router를 제외한 banner 완성.

--- electric/src/App.js
+++ electric/src/App.js
@@ -1,8 +1,32 @@
-import './App.css';
+import { createGlobalStyle} from 'styled-components';
+import {RecoilRoot} from 'recoil';
+import Banner from './component/banner/Banner';
 
+const GlobalStyle = createGlobalStyle`
+  *{
+    scroll-behavior:smooth;
+    margin : 0px;
+    padding : 0px;
+    box-sizing : border-box;
+    input:focus {
+      outline: none
+    };
+    textarea:focus {
+      outline: none;
+    };
+  }
+`
 function App() {
   return (
-    <div></div>
+    <RecoilRoot>
+      <div className='App'>
+        <GlobalStyle />
+        {
+          <Banner />
+        }
+
+      </div>
+    </RecoilRoot>
   );
 }
 
--- electric/src/component/banner/Banner.jsx
+++ electric/src/component/banner/Banner.jsx
@@ -1,12 +1,55 @@
 import React from "react";
-import styled from 'styled-component';
+import styled from 'styled-components';
 
+const Snav = styled.div`
+    height: 100px;
+    width: 100vw;
+    display: flex;
+    background-color:#4FCBF2;
+    opacity: 0.7;
+    justify-content: space-between;
+`
+
+const Smain = styled.div`
+    margin-left: 15px;
+    text-align: center;
+    vertical-align:middle;
+    display:flex;
+    align-items: center;
+`
+
+const Sul = styled.ul`
+    list-style-type: none;
+    display: flex;
+`
+
+const Sli = styled.li`
+    display:flex;
+    align-items: center;
+    margin-right: 20px;
+    }
+`
+
+const Stext = styled.div`
+    color: white;
+    font-size: xx-large;
+        :hover{
+            cursor: pointer;
+            color: rgb(230, 230, 230);
+        }
+`
 
 const Banner = () =>{
-
-
     return(
         <>
+        <Snav>
+        <Smain><Stext>Electricity</Stext></Smain>
+        <Sul>
+            <Sli><Stext>연구과정</Stext></Sli>
+            <Sli><Stext>그래프 예측</Stext></Sli>
+            <Sli><Stext>참고 자료</Stext></Sli>
+        </Sul>
+        </Snav>
         </>
     )
 
