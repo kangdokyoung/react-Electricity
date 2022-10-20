5183b91fb9f8b21ba377216253a8ff3eac4f27d0
Dokyoung <ehrud6672@naver.com>
2022-10-20 16:31:07 +0900

    express 설정 데이터베이스 데이터 입력

---
+++ electric/server/express.js
@@ -0,0 +1,20 @@
+//----------------------------------------express 실행
+const express = require('express');
+const app = express();
+const port = process.env.port||3005;
+//------------------------------------------mysql 연결
+const mysql = require('mysql');
+const {mysqlKey} = require('./mysqlKey.js');
+const connection = mysql.createConnection(mysqlKey);
+connection.connect();
+//------------------------------------------helmet 페이지 보안
+const helmet = require('helmet');
+app.use(helmet());
+//------------------------------------------cors 허가
+const cors = require('cors');
+app.use(cors({
+    origin : true, credentials: true
+}));
+//------------------------------------------서버 구동
+app.listen(port, ()=>{console.log(`server run in ${port}`)});
+//------------------------------------------파일 임포트
